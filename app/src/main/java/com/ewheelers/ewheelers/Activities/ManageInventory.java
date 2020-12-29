9cee340904dcdc58a13b8a453e76ae68354b2ba5
Gopi Panguluri <gopipanguluri@gmail.com>
2020-12-29 23:12:30 +0530

    updated 29/12/2020

--- app/src/main/java/com/ewheelers/ewheelers/Activities/ManageInventory.java
+++ app/src/main/java/com/ewheelers/ewheelers/Activities/ManageInventory.java
@@ -1,19 +1,27 @@
 package com.ewheelers.ewheelers.Activities;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.app.Activity;
 import android.content.Context;
+import android.os.Build;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewTreeObserver;
+import android.view.animation.AnimationUtils;
+import android.view.animation.LayoutAnimationController;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.android.volley.AuthFailureError;
@@ -39,6 +47,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import static android.view.View.GONE;
+
 public class ManageInventory extends AppCompatActivity implements TextWatcher {
     RecyclerView recyclerView;
     InventoryAdapter inventoryAdapter;
@@ -46,11 +56,23 @@ public class ManageInventory extends AppCompatActivity implements TextWatcher {
     List<InventoryModel> inventoryModels = new ArrayList<>();
     EditText searchProd;
     Button button;
+    SwipeRefreshLayout swipeRefreshLayout;
+    private int pagenumber;
+    private int itemcount = 10;
+    private int visibleitemcount, totalitemcount, pastvisibleitems;
+    private boolean loading = true;
+    ProgressBar progressBar;
 
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manage_inventory);
+        progressBar = findViewById(R.id.progres);
+        swipeRefreshLayout = findViewById(R.id.swipelayout);
+        swipeRefreshLayout.setColorSchemeColors(getResources().getColor(R.color.colorAccent, null));
+        pagenumber = 1;
+
         recyclerView = findViewById(R.id.list_of_prods);
         searchProd = findViewById(R.id.search_prod);
         searchProd.addTextChangedListener(this);
@@ -58,16 +80,60 @@ public class ManageInventory extends AppCompatActivity implements TextWatcher {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                getList(searchProd.getText().toString());
-                hideKeyboardFrom(getApplicationContext(),v);
+                getList(searchProd.getText().toString(), pagenumber);
+                hideKeyboardFrom(getApplicationContext(), v);
             }
         });
-        getList("");
+
+        getList(searchProd.getText().toString(), pagenumber);
+        if (inventoryModels != null) {
+            inventoryAdapter = new InventoryAdapter(getApplicationContext(), inventoryModels);
+            final RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext(), RecyclerView.VERTICAL, false);
+            recyclerView.setLayoutManager(layoutManager);
+            recyclerView.setAdapter(inventoryAdapter);
+
+            swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+                @Override
+                public void onRefresh() {
+                    pagenumber = 1;
+                    loading = true;
+                    getList(searchProd.getText().toString(), pagenumber);
+                }
+            });
+
+            recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+                @Override
+                public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
+                    super.onScrolled(recyclerView, dx, dy);
+                    if(dy > 0)
+                    {
+                        visibleitemcount = layoutManager.getChildCount();
+                        totalitemcount = layoutManager.getItemCount();
+                        pastvisibleitems = ((LinearLayoutManager) layoutManager).findFirstVisibleItemPosition();
+
+//if loading is true which means there is data to be fetched from the database
+
+                        if(loading)
+                        {
+                            if((visibleitemcount + pastvisibleitems) >= totalitemcount)
+                            {
+                                progressBar.setVisibility(View.VISIBLE);
+                                swipeRefreshLayout.setRefreshing(true);
+                                loading = false;
+                                pagenumber += 1;
+                                getList(searchProd.getText().toString(),pagenumber);
+                            }
+                        }
+                    }
+                }
+            });
+        }
     }
 
 
-    public void getList(String s) {
-        inventoryModels.clear();
+    public void getList(String s, int pagenumer) {
+        //inventoryModels.clear();
+        swipeRefreshLayout.setRefreshing(false);
         final RequestQueue queue = Volley.newRequestQueue(ManageInventory.this);
         String serverurl = API.sellerProducts;
         StringRequest stringRequest = new StringRequest(Request.Method.POST, serverurl, new com.android.volley.Response.Listener<String>() {
@@ -78,6 +144,7 @@ public class ManageInventory extends AppCompatActivity implements TextWatcher {
                     String status = jsonObject.getString("status");
                     String msg = jsonObject.getString("msg");
                     if (status.equals("1")) {
+                        progressBar.setVisibility(GONE);
                         JSONObject jsonData = jsonObject.getJSONObject("data");
                         JSONArray jsonArray = jsonData.getJSONArray("arrListing");
                         if (jsonArray.length() == 0) {
@@ -98,12 +165,17 @@ public class ManageInventory extends AppCompatActivity implements TextWatcher {
                                 inventoryModel.setStatusOfprod(statusOfprod);
                                 inventoryModels.add(inventoryModel);
                             }
-                            LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getApplicationContext(), RecyclerView.VERTICAL, false);
+                            /*LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getApplicationContext(), RecyclerView.VERTICAL, false);
                             recyclerView.setLayoutManager(linearLayoutManager);
                             inventoryAdapter = new InventoryAdapter(getApplicationContext(), inventoryModels);
                             recyclerView.setAdapter(inventoryAdapter);
-                            inventoryAdapter.notifyDataSetChanged();
-
+                            inventoryAdapter.notifyDataSetChanged();*/
+                            if (!inventoryModels.isEmpty()) {
+                                loading = true;
+                                swipeRefreshLayout.setRefreshing(false);
+                            } else {
+                                Toast.makeText(getApplicationContext(), "no more data available...", Toast.LENGTH_LONG).show();
+                            }
                         }
 
                     } else {
@@ -113,14 +185,15 @@ public class ManageInventory extends AppCompatActivity implements TextWatcher {
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
-
+                inventoryAdapter.notifyDataSetChanged();
             }
         }, new com.android.volley.Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
                 VolleyLog.d("Main", "Error: " + error.getMessage());
                 Log.d("Main", "" + error.getMessage() + "," + error.toString());
-
+                swipeRefreshLayout.setRefreshing(false);
+                progressBar.setVisibility(GONE);
             }
         }) {
 
@@ -135,7 +208,11 @@ public class ManageInventory extends AppCompatActivity implements TextWatcher {
             @Override
             public Map<String, String> getParams() {
                 Map<String, String> data3 = new HashMap<String, String>();
-                data3.put("keyword", s);
+                if (!s.isEmpty() || s != null) {
+                    data3.put("keyword", s);
+                }
+                data3.put("page", String.valueOf(pagenumer));
+                data3.put("pageCount", String.valueOf(itemcount));
                 return data3;
             }
 
@@ -154,8 +231,8 @@ public class ManageInventory extends AppCompatActivity implements TextWatcher {
 
     @Override
     public void onTextChanged(CharSequence s, int start, int before, int count) {
-        if(count==0){
-            getList("");
+        if (count == 0) {
+            getList("", pagenumber);
         }
     }
 
